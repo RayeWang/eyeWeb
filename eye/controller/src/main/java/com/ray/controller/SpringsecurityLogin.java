f04fca2f78b477751b6789edbd523d1578009889
RayWang <759039391@qq.com>
2015-05-28 13:20:19 +0800

    生成了用户实体

---
+++ eye/controller/src/main/java/com/ray/controller/SpringsecurityLogin.java
@@ -0,0 +1,9 @@
+package com.ray.controller;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringsecurityLogin extends UsernamePasswordAuthenticationFilter {
+
+}
