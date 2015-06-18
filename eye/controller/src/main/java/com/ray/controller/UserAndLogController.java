9b2fbcdbd8fca1473b32a4b2c7c6dea17c1ec978
RayWang <759039391@qq.com>
2015-06-18 17:00:39 +0800

    修改了权限控制的，但是修改密码还是有问题

--- eye/controller/src/main/java/com/ray/controller/UserAndLogController.java
+++ eye/controller/src/main/java/com/ray/controller/UserAndLogController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
