a7cdf6baac927c9572536b4f86c7e4025fc731f4
Ray Wang <759039391@qq.com>
2015-06-22 16:53:52 +0800

    之前的获取到的是127.0.0.1，现在试试这个能否获取到真实IP

--- eye/controller/src/main/java/com/ray/controller/admin/LoginSuccessHandler.java
+++ eye/controller/src/main/java/com/ray/controller/admin/LoginSuccessHandler.java
@@ -103,12 +103,13 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {    
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");    
         }    
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {    
-            ip = request.getRemoteAddr();    
-        }
         if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)){
         	ip = request.getHeader("WL-Proxy-Client-IP"); 
         }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {    
+            ip = request.getRemoteAddr();    
+        }
+        
         if(ip == null || ip.length() == 0){
         	return "未知IP";
         }
